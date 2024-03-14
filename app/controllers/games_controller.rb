6000c2e93edb418fd3d9bfc65cf87934b9de41f0
ginpan <zgpan7@gmail.com>
2024-03-14 21:45:09 +1100

    TBC

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do
+      @letters << ('a'..'z').to_a.sample
+    end
+  end
+
+  def score
+    raise
+  end
+end
